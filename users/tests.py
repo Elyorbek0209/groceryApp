b87f2341ca4d2b3ef9b537cd20840f46a1ee80d0
Elyorbek <elyorbek.soliev@gmail.com>
2020-05-10 18:09:21 -0400

    initial checkin

---
+++ users/tests.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+# Create your tests here.
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='elyor',
+            email='elyorbeksoliev@gmail.com',
+            password='elyor'
+        )
+
+        self.assertEqual(user.username, 'elyor')
+        self.assertEqual(user.email, 'elyorbeksoliev@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    
+    def test_create_superuser(self):
+        User=get_user_model()
+        admin_user = User.objects.create_superuser(
+            username = 'superadmin',
+            email='superadmin@gmail.com',
+            password = 'testpass123'
+        )
+
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        
+
+
+
+
+
+
+
+
