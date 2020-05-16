a4d19989fee2d1a8075321e12b6c627a4e892e5b
Elyorbek <elyorbek.soliev@gmail.com>
2020-05-16 17:08:33 -0400

    test

--- grocery_project/urls.py
+++ grocery_project/urls.py
@@ -19,10 +19,15 @@ from django.urls import path, include
 urlpatterns = [
     # Django Admin
     path('admin/', admin.site.urls),
-    
+
+    #User management
+    path('accounts/', include('django.contrib.auth.urls')), 
+
+
     #Local apps
     path('', include('pages.urls')), 
+    path('accounts/', include('users.urls')), # new
+
     
-    #User management
-    path('accounts/', include('django.contrib.auth.urls')) # new
+
 ]
